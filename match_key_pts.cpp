33aba4372a03912f6b9df16485f1b12771dfe941
Trung <tnguyen2@oberlin.edu>
2022-05-11 22:21:24 -0400

    Add match_key_pts.cpp

---
+++ match_key_pts.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include "main.h"
+
+using namespace std;
+using namespace cv;
+
+int baseline;
+int width;
+int height;
+
+Vec2b* match_key_pts(Vec2b* left_key_pts, Vec2b* right_key_pts, Mat left_img, Mat right_img) {		//malloc
+	return NULL;
+}
+
+
