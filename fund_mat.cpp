d8d61a15dc1eed32463d47ca738fe1611fed9d10
DDonut <macbook@Brian.local>
2022-05-30 10:29:57 -0400

    Fund_mat update

--- fund_mat.cpp
+++ fund_mat.cpp
@@ -17,26 +17,30 @@ using namespace cv;
 using namespace std;
 
 Mat lhs_mat(Point arr1[9], Point arr2[9]) {
-    Mat lhs = Mat::zeros(9, 9, CV_32SC1);
+    Mat lhs = Mat::zeros(9, 9, CV_64FC1);
     for (int i = 0; i < 9; i++) {
-        lhs.at<int>(i, 0) += ((int) arr1[i].x) * ((int) arr2[i].x);
-        lhs.at<int>(i, 1) += ((int) arr1[i].x) * ((int) arr2[i].y);
-        lhs.at<int>(i, 2) += (int) arr1[i].x;
-        lhs.at<int>(i, 3) += ((int) arr1[i].y) * ((int) arr2[i].x);
-        lhs.at<int>(i, 4) += ((int) arr1[i].y) * ((int) arr2[i].y);
-        lhs.at<int>(i, 5) += (int) arr1[i].y;
-        lhs.at<int>(i, 6) += (int) arr2[i].x;
-        lhs.at<int>(i, 7) += (int) arr2[i].y;
-        lhs.at<int>(i, 8) += 1;
+        lhs.at<float>(i, 0) += ((int) arr1[i].x) * ((int) arr2[i].x);
+        lhs.at<float>(i, 1) += ((int) arr1[i].x) * ((int) arr2[i].y);
+        lhs.at<float>(i, 2) += (int) arr1[i].x;
+        lhs.at<float>(i, 3) += ((int) arr1[i].y) * ((int) arr2[i].x);
+        lhs.at<float>(i, 4) += ((int) arr1[i].y) * ((int) arr2[i].y);
+        lhs.at<float>(i, 5) += (int) arr1[i].y;
+        lhs.at<float>(i, 6) += (int) arr2[i].x;
+        lhs.at<float>(i, 7) += (int) arr2[i].y;
+        lhs.at<float>(i, 8) += 1;
     }
     return lhs;
 }
 
 
 Mat fundamental(Mat lhs) {
-    Mat rhs = Mat::zeros(9 , 1, CV_32SC1);
+    Mat rhs = Mat::zeros(9 , 1, CV_64FC1);
+    //rhs.at<float>(8,0) += 9;
     Mat sub_fund = Mat::zeros(9 , 1, CV_64FC1);
-    solve(lhs, rhs, sub_fund);
+    // Mat inv_mat;
+    // invert(lhs, inv_mat)
+    SVD::solveZ(lhs, sub_fund);
+
     Mat fund = Mat::zeros(3 , 3, CV_64FC1);
     fund.at<float>(0,0) += sub_fund.at<float>(0,0);
     fund.at<float>(0,1) += sub_fund.at<float>(1,0);
