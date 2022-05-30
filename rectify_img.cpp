4ff93a3d34168b0682b0cb610bae926cfbf05d37
DDonut <macbook@Brian.local>
2022-05-30 10:30:44 -0400

    Rect

--- rectify_img.cpp
+++ rectify_img.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <algorithm>
 #include <tuple>
-#include <opencv2\opencv.hpp>
+
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2\core\core.hpp>
@@ -35,21 +35,22 @@ float find_theta(Point &center, Point &spin){
 }
 
 Mat rot_mat(float theta){
-    Mat rot = Mat::zeros(2, 2, CV_64FC1);
-    rot.at<float>(0, 0) += cos((2 * M_PI) - theta);
-    rot.at<float>(0, 1) -= sin((2 * M_PI) - theta);
-    rot.at<float>(1, 0) += sin((2 * M_PI) - theta);
-    rot.at<float>(1, 1) += cos((2 * M_PI) - theta); 
+    Mat rot = Mat::zeros(2, 2, CV_32FC1);
+    rot.at<float>(0, 0) += cos((2.0 * M_PI) - theta);
+    rot.at<float>(0, 1) -= sin((2.0 * M_PI) - theta);
+    rot.at<float>(1, 0) += sin((2.0 * M_PI) - theta);      
+    rot.at<float>(1, 1) += cos((2.0 * M_PI) - theta); 
     return rot;
 }
 
 tuple<Mat, Mat> rectify(Mat &img1, Mat &img2, Mat &fund){
     Point lpoint1((int) (img1.rows / 2), (int) (img1.cols / 2));
+
     Vec3f right_line = create_line_right(fund, lpoint1);
     int rp1_x = (int) (img2.rows /2);
     int rp1_y = floor(- ((right_line[2] + (right_line[0] * rp1_x)) / right_line[1]));
     Point rpoint1(rp1_x, rp1_y);
-    int rp2_x = floor(rp1_x + (0.2 * img2.rows));
+    int rp2_x = (int) (img2.rows /1.5);
     int rp2_y = floor(- ((right_line[2] + (right_line[0] * rp2_x)) / right_line[1]));
     Point rpoint2(rp2_x, rp2_y);
     Mat right_rot = rot_mat(find_theta(rpoint1, rpoint2));
@@ -61,16 +62,17 @@ tuple<Mat, Mat> rectify(Mat &img1, Mat &img2, Mat &fund){
     int lp3_x = (int) (img1.rows / 2);
     int lp3_y = floor(- ((left_line[2] + (left_line[0] * lp3_x)) / left_line[1]));
     Point lpoint3(lp3_x, lp3_y);
-    Mat left_rot = rot_mat(find_theta(lpoint2, lpoint3));
+    Mat left_rot;
+    if (lp3_x > lp2_x) left_rot = rot_mat(find_theta(lpoint2, lpoint3));
+    else left_rot = rot_mat(find_theta(lpoint3, lpoint2));
 
     Mat new_img1(img1.rows, img1.cols, CV_8UC3, Vec3b(0, 0, 0));
     for (int i = 0; i < img1.rows; i++){
         for (int j = 0; j < img1.cols; j++){
-            Vec2i tmp(i, j);
-            Mat tmp_left = left_rot;
-            tmp_left.mul(tmp);
-            if ((tmp_left.at<float>(0, 0) >= 0) && (tmp_left.at<float>(0,0) < img1.rows) && (tmp_left.at<float>(1, 0) >= 0) && (tmp_left.at<float>(1,0) < img1.cols)) {
-                new_img1.at<Vec3b>((int) tmp_left.at<float>(1,0) , (int) tmp_left.at<float>(0,0)) = img1.at<Vec3b>(i ,j);
+            Mat tmp = (Mat_<float>(2, 1) << i, j);
+            Mat loc = left_rot * tmp;
+            if ((loc.at<float>(0, 0) >= 0) && ((int) loc.at<float>(0, 0) < img1.rows) && (loc.at<float>(1, 0) >= 0) && ((int) loc.at<float>(1,0) < img1.cols)) {
+                new_img1.at<Vec3b>((int) loc.at<float>(0,0) , (int) loc.at<float>(1,0)) = img1.at<Vec3b>(i, j);
             }
         }
     }
@@ -78,15 +80,26 @@ tuple<Mat, Mat> rectify(Mat &img1, Mat &img2, Mat &fund){
     Mat new_img2(img2.rows, img2.cols, CV_8UC3, Vec3b(0, 0, 0));
     for (int i = 0; i < img2.rows; i++){
         for (int j = 0; j < img2.cols; j++){
-            Vec2i tmp(i, j);
-            Mat tmp_right = right_rot;
-            tmp_right.mul(tmp);
-            if ((tmp_right.at<float>(0, 0) >= 0) && (tmp_right.at<float>(0,0) < img2.rows) && (tmp_right.at<float>(1, 0) >= 0) && (tmp_right.at<float>(1,0) < img2.cols)) {
-                new_img2.at<Vec3b>((int) tmp_right.at<float>(1,0) , (int) tmp_right.at<float>(0,0)) = img2.at<Vec3b>(i ,j);
+            Mat tmp = (Mat_<float>(2, 1) << i, j);
+            Mat loc = right_rot * tmp;
+            
+            if ((loc.at<float>(0, 0) >= 0) && ((int) loc.at<float>(0, 0) < img2.rows) && (loc.at<float>(1, 0) >= 0) && ((int) loc.at<float>(1,0) < img2.cols)) {
+                new_img2.at<Vec3b>((int) loc.at<float>(0,0) , (int) loc.at<float>(1,0)) = img2.at<Vec3b>(i, j);
             }
         }
     }
 
     return make_tuple(new_img1, new_img2);
+}
 
+int main(int argc, char **argv)
+{
+    Mat image_left = imread("left.bmp");
+    Mat image_right = imread("right.bmp");
+    Mat fund = (Mat_<float>(3, 3) << -.003, -.028, 13.19, -.003, -.008, -29.2, 2.97, 50, -9999);
+    tuple<Mat, Mat> ret = rectify(image_left, image_right, fund);
+    imshow("left", get<0>(ret));
+    waitKey(0);
+    imshow("right", get<1>(ret));
+    waitKey(0);
 }
